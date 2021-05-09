6521e0a9651eb97b0f9c6a828be408400eb59542
Yacine Merzouk <yacine@merzouk.ca>
2021-05-09 11:25:35 -1000

    Initial public release

--- package.js
+++ package.js
@@ -1,21 +1,21 @@
 Package.describe({
-  name: 'iron:core',
-  summary: 'Iron namespace and utilities.',
-  version: '1.0.11',
-  git: 'https://github.com/iron-meteor/iron-core'
+  name: 'meteorhubdotnet:iron-core',
+  summary: 'Fork of iron:core for Firestorm',
+  version: '1.0.0',
+  git: 'https://github.com/meteorhubdotnet/firestorm-iron-core'
 });
 
 Package.on_use(function (api) {
-  api.versionsFrom('METEOR@0.9.2');
+  api.versionsFrom('METEOR@2.2');
 
   // dependencies
   api.use('underscore');
   api.use('ejson'); // for cloning
 
   // remove migrated version
-  api.use('cmather:iron-core@0.2.0', {weak: true});
+  // api.use('cmather:iron-core@0.2.0', {weak: true});
 
-  api.add_files('lib/version_conflict_error.js');
+  // api.add_files('lib/version_conflict_error.js');
   api.add_files('lib/iron_core.js');
 
   // symbol exports
@@ -23,7 +23,7 @@ Package.on_use(function (api) {
 });
 
 Package.on_test(function (api) {
-  api.use('iron:core');
+  api.use('meteorhubdotnet:iron-core');
   api.use('tinytest');
   api.use('test-helpers');
   api.add_files('test/iron_core_test.js');
