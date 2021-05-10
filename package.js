9ea5187acdd10292fda4e9b6950fe5eeb51aa2c4
Yacine Merzouk <yacine@merzouk.ca>
2021-05-09 16:26:07 -1000

    Updated version and constraints

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'meteorhubdotnet:firestorm-iron-core',
   summary: 'Fork of iron:core for Firestorm',
-  version: '1.0.0',
+  version: '1.0.4',
   git: 'https://github.com/meteorhubdotnet/firestorm-iron-core'
 });
 
