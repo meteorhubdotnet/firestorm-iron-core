eceb8b18ffdcf6d5255bb7e1a0a6c0bd308c75ac
Yacine Merzouk <yacine@merzouk.ca>
2021-09-18 15:28:39 -1000

    Updated for Meteor 2.3.6

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: 'meteorhubdotnet:firestorm-iron-core',
   summary: 'Fork of iron:core for Firestorm',
-  version: '1.0.4',
+  version: '1.0.5',
   git: 'https://github.com/meteorhubdotnet/firestorm-iron-core'
 });
 
-Package.on_use(function (api) {
-  api.versionsFrom('METEOR@2.2');
+Package.onUse(function (api) {
+  api.versionsFrom('METEOR@2.3.6');
 
   // dependencies
   api.use('underscore');
@@ -15,16 +15,16 @@ Package.on_use(function (api) {
   // remove migrated version
   // api.use('cmather:iron-core@0.2.0', {weak: true});
 
-  // api.add_files('lib/version_conflict_error.js');
-  api.add_files('lib/iron_core.js');
+  // api.addFiles('lib/version_conflict_error.js');
+  api.addFiles('lib/iron_core.js');
 
   // symbol exports
   api.export('Iron');
 });
 
-Package.on_test(function (api) {
+Package.onTest(function (api) {
   api.use('meteorhubdotnet:firestorm-iron-core');
   api.use('tinytest');
   api.use('test-helpers');
-  api.add_files('test/iron_core_test.js');
+  api.addFiles('test/iron_core_test.js');
 });
